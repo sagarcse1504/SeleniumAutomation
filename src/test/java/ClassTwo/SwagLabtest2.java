b45db3d19e7de3541aac606092348807d408fe3e
Sagar <sagarcse1504@gmail.com>
2024-05-26 19:27:34 +0700

    selenium 2nd class

--- src/test/java/ClassTwo/SwagLabtest2.java
+++ src/test/java/ClassTwo/SwagLabtest2.java
@@ -2,9 +2,12 @@ package ClassTwo;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class SwagLabTesting {
+import java.util.List;
+
+public class SwagLabtest2 {
     public static void main(String[] args) throws InterruptedException {
         WebDriver driver = new FirefoxDriver();
         driver.get("https://www.saucedemo.com/");
@@ -13,18 +16,23 @@ public class SwagLabTesting {
         System.out.println("==============Web Information==============");
         System.out.println(driver.getCurrentUrl());
         System.out.println(driver.getTitle());
-        driver.findElement(By.id("user-name")).sendKeys("standard_user");
-        Thread.sleep(2000);
 
-        driver.findElement(By.id("password")).sendKeys("secret_sauce");
-        Thread.sleep(2000);
+        List<WebElement> webElements = driver.findElements(By.tagName("a"));
 
-        driver.findElement(By.id("login-button")).click();
+        for (WebElement element: webElements){
+            System.out.println(element);
+        }
 
-        System.out.println("Successfully Login");
+        driver.findElement(By.name("user-name")).sendKeys("standard_user");
+
+        driver.findElement(By.name("password")).sendKeys("secret_sauce");
+//        Thread.sleep(2000);
+        driver.findElement(By.name("login-button")).click();
+
 
         Thread.sleep(2000);
         driver.quit();
+
 
     }
 }
