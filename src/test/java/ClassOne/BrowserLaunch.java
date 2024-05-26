80e4b7b22cfed21513cddc490ff01717176963a7
Sagar <sagarcse1504@gmail.com>
2024-05-26 15:02:31 +0700

    first commit

--- src/test/java/ClassOne/BrowserLaunch.java
+++ src/test/java/ClassOne/BrowserLaunch.java
@@ -7,6 +7,9 @@ public class FirstClass {
     public static void main(String[] args) throws InterruptedException {
         WebDriver  driver = new ChromeDriver();
         driver.get("https://www.google.com/");
+        System.out.println(driver.getTitle());
+        System.out.println(driver.getCurrentUrl());
+
         Thread.sleep(3000);
         driver.quit();
     }
